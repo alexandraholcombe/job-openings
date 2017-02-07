3ef7d917ec5e4affe094f989a677b1189c48cbcc
Alexandra Holcombe and Minh Phuong <alexandra+minh@>
2017-02-07 14:23:36 -0800

    Add getters and setters

--- Objects/JobOpenings.cs
+++ Objects/JobOpenings.cs
@@ -9,5 +9,46 @@ namespace JobOpenings.Objects
     private string _description;
     private string _email;
     private string _contact;
+
+    public Job(string title, string description, string email, string contact)
+    {
+      _title = title;
+      _description = description;
+      _email = email;
+      _contact = contact;
+    }
+
+    public string GetTitle()
+    {
+      return _title;
+    }
+    public void SetTitle(string inputTitle)
+    {
+      _title = inputTitle;
+    }
+    public string GetDescription()
+    {
+      return _description;
+    }
+    public void SetDescription(string inputDescription)
+    {
+      _description = inputDescription;
+    }
+    public string GetEmail()
+    {
+      return _email;
+    }
+    public void SetEmail(string inputEmail)
+    {
+      _email = inputEmail;
+    }
+    public string GetContact()
+    {
+      return _contact;
+    }
+    public void SetContact(string inputContact)
+    {
+      _contact = inputContact;
+    }
   }
 }
