ea2443ccf225c0660b17a6b4fb3f5db341be9276
Alexandra Holcombe and Minh Phuong <alexandra+minh@>
2017-02-07 14:19:21 -0800

    Create add_openings.cshtml, all_openings.cshtml, openings_added.cshtml, add class job in Objects/JobOpenings.cs

---
+++ Objects/JobOpenings.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+namespace JobOpenings.Objects
+{
+  public class Job
+  {
+    private string _title;
+    private string _description;
+    private string _email;
+    private string _contact;
+  }
+}
